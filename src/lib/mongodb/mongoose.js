9a854eef31ba669e7c4241bf29447173950080fa
M-RaeesDev <raeesawan361@gmail.com>
2025-07-27 19:46:56 +0500

    Added MongoDB Connection

---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let initialized = false;
+
+export const connet = async ()=>{
+    mongoose.set('strictQuery', true)
+
+    if(initialized){
+        console.log("MongoDB Is Already Connected");
+        return;   
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,{
+
+            dbName:'naxt auth app',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("MongoDB Connected");
+        initialized = true;
+    } catch (error) {
+        console.log("MongoDB Connection Error :", error);
+        
+    }
+}
