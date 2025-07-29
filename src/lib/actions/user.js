d6d7a5b04a7d3f9b68944630961807fed9f1ae4d
M-RaeesDev <raeesawan361@gmail.com>
2025-07-29 22:45:46 +0500

    Added User Actions Create, Update and Delete.

---
+++ src/lib/actions/user.js
@@ -0,0 +1,42 @@
+import User from "../models/user.model";
+import { connect } from "../mongodb/mongoose";
+
+export const createOrUpdateUser = async (
+  id,
+  first_nanme,
+  last_name,
+  image_url,
+  email_adresses,
+  username
+) => {
+  try {
+    await connect();
+
+    const user = await User.findOneAndUpdate(
+      { clerkId: id },
+      {
+        $set: {
+          firstName: first_nanme,
+          lastName: last_name,
+          avatar: image_url,
+          email: email_adresses[0].email,
+          userName: username,
+        },
+      },
+      { new: true, upsert: true }
+    );
+    return user;
+  } catch (error) {
+    console.log("Error creating or updating user: ", error);
+  }
+};
+
+export const deleteUser = async (id) => {
+  try {
+    await connect();
+
+    User.findOneAndDelete({ clerkId: id });
+  } catch (error) {
+    console.log("Error deleting User: ", error);
+  }
+};
