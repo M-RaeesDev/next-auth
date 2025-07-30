bb47f697f4dbfdc0fd1d58b2a77697e64411e5e3
M-RaeesDev <raeesawan361@gmail.com>
2025-07-30 23:36:55 +0500

    Create Upadate and Delete User Data Through Clerk in MongoDB

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,3 +1,4 @@
+import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 
 export async function POST(req) {
@@ -7,18 +8,40 @@ export async function POST(req) {
 
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = evt.data;
-    const eventType = evt.type;
+    const { id } = evt?.data;
+    const eventType = evt?.type;
     console.log(
       `Received webhook with ID ${id} and event type of ${eventType}`
     );
-    console.log("Webhook payload:", evt.data);
+    console.log("Webhook payload:", evt?.data);
 
-    if (evt.type === "user.created") {
-      console.log("Create_userId:", evt.data.id);
+    if (eventType === "user.created" || eventType === "user.updated") {
+      const {id, first_name, last_name, image_url, email_adresses, username} = evt?.data;
+      try {
+        await createOrUpdateUser(
+          id,
+          first_name,
+          last_name,
+          image_url,
+          email_adresses,
+          username
+        );
+        return new Response('User is Created or Updated :', {status:200,});
+      } catch (error) {
+        console.log("Error Creating and Updating User: ", error);
+        return new Response ('Error Occured', {status:400,});
+        
+      }
     }
-    if (evt.type === "user.updated") {
-      console.log("update_userId:", evt.data.id);
+    if (eventType === "user.deleted") {
+      const {id} = evt?.data;
+      try {
+        await deleteUser(id);
+        return new Response("User Deleted: ", {status: 200,})
+      } catch (error) {
+        console.log("Error Delete User", error);
+        return new Response('Error Occured', {status: 400,})
+      }
     }
 
     return new Response("Webhook received", { status: 200 });
